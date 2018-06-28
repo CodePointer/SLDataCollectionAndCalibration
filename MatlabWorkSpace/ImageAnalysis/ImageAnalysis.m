c12677d8ba0f499901661af9f7d1564026576db1
汝坤 乔 <rukunqiao@outlook.com>
2018-06-28 17:27:09 +0800

    Add depth range estimation part

--- MatlabWorkSpace/ImageAnalysis/ImageAnalysis.m
+++ MatlabWorkSpace/ImageAnalysis/ImageAnalysis.m
@@ -1,36 +1,6 @@
-frm_idx = 1;
+warning('off');
+main_file_path = 'E:/SLDataSet/20180626/HandMove_BlkDis/';
 
-raw_img = imread(['dyna_mat', num2str(frm_idx), '.png']);
-
-hsv_img = rgb2hsv(raw_img);
-
-hsv_img(:, :, 2) = 1;
-for h = 1:1024
-  for w = 1:1280
-    if hsv_img(h, w, 3) > 0.2
-      hsv_img(h, w, 3) = 1.0;
-    else
-      hsv_img(h, w, 3) = 0.0;
-    end
-  end
+for frm_idx = 0:300
+  KMeansClassfication;
 end
-
-imshow(hsv2rgb(hsv_img));
-
-rgb_img = hsv2rgb(hsv_img);
-
-thred = [0.6, 0.5, 0.05];
-
-new_rgb_img = zeros(1024, 1280);
-for h = 1:1024
-  for w = 1:1280
-    for c = 1:3
-      if rgb_img(h, w, c) > thred(c)
-        new_rgb_img(h, w, c) = 1.0;
-      else
-        new_rgb_img(h, w, c) = 0.0;
-      end
-    end
-  end
-end
-imshow(new_rgb_img);
